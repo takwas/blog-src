1c36afad2fff09374e522f5899ec5784425c8998
Tosin Damilare James Animashaun <acetakwas@gmail.com>
2015-12-21 14:57:58 +0100

    Fix syntax error

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ from hashlib import md5
 
 
 SITENAME = u'acetakwas.log();'
-DESCRIPTION = u"while True: print    <strong>\\<strong>"
+DESCRIPTION = u"while True: print    <strong>\\</strong>"
 SITE_TAGLINE = DESCRIPTION
 SITE_SOURCE = u'https://github.com/takwas/blog-src'
 
